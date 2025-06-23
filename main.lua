2b27208c767b9ff51c7a1d86e0ce410f5a9f51a9
l7n-n <znrruni@gmail.com>
2025-06-23 07:01:24 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,6 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+RegisterNetEvent('gangsystem:refresh')
+AddEventHandler('gangsystem:refresh', function(data)
+    -- تحديثات العميل (اختياري)
+end)
